019ac2abb56c792b0e38d611ab3ee3a2cd102a27
studentzzzx <zzzstudenta@gmail.com>
2025-11-26 13:32:10 +0700

    Add files via upload

---
+++ Insuree.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarInsurance.Models
+{
+    public class Insuree
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public bool DUI { get; set; }
+        public int SpeedingTickets { get; set; }
+        public bool CoverageType { get; set; }
+        public decimal Quote { get; set; }
+    }
+}
